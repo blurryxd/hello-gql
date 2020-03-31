33fcdecfea46a593f63f89d1c4a939e5dc22a569
Juho <juho.huhtanen@windowslive.com>
2020-03-31 11:53:53 +0300

    update

--- server.js
+++ server.js
@@ -7,30 +7,27 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const MyGraphQLSchema = require('./schema/schema');
 const port = 3000;
-const app = express();
 const cors = require('cors');
 const authRoute = require('./routes/authRoute');
 const passport = require('./utils/pass');
+
+const app = express();
+
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 
-// dummy function to set user (irl: e.g. passport-local)
-const auth = (req, res, next) => {
-    req.user = true;
-    next();
-};
-
 // dummy function to check authentication (irl: e.g. passport-jwt)
 const checkAuth = (req, res) => {
-    console.log('user', req.user);
-    if (!req.user)
-        throw new Error('Not authenticated');
+    passport.authenticate('jwt', {session: false}, (err, user)=>{
+        if(err || !user){
+            throw new Error('Not authed');
+        }
+    })(req, res);
 };
 
-app.post(auth);
 app.use('/auth', authRoute);
 
 
